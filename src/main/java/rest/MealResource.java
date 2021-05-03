57fe33a22712e6e94b84b522c365579be8bce79e
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2021-05-03 18:43:02 +0200

    feat(MealResource): get all meals endpoint setup + generate all meals

---
+++ src/main/java/rest/MealResource.java
@@ -0,0 +1,55 @@
+package rest;
+
+import dtos.MealDTO;
+import dtos.RestaurantDTO;
+import entities.meal.MealRepository;
+import facades.MealFacade;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import rest.provider.Provider;
+
+@Path("meals")
+public class MealResource extends Provider {
+
+    private static final MealRepository REPO = MealFacade.getInstance(EMF);
+
+    @Override
+    public Response getAll() {
+        List<MealDTO> mealDTOS = REPO.getAllMeals();
+        RestaurantDTO restaurantDTO = new RestaurantDTO(mealDTOS);
+        return Response.ok(GSON.toJson(restaurantDTO)).build();
+    }
+
+    @GET
+    @Path("/generate")
+    public Response generateAllMeals() {
+        REPO.generateAllMeals();
+        return Response.ok().build();
+    }
+
+    @Override
+    public Response getById(int id) {
+        //TODO (tz): implement this!
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+    @Override
+    public Response create(String jsonBody) {
+        //TODO (tz): implement this!
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+    @Override
+    public Response update(int id, String jsonBody) {
+        //TODO (tz): implement this!
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+    @Override
+    public Response delete(int id) {
+        //TODO (tz): implement this!
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+}
---
+++ src/test/java/rest/MealResourceTest.java
@@ -0,0 +1,122 @@
+package rest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import dtos.MealDTO;
+import entities.meal.Category;
+import entities.meal.Meal;
+import io.restassured.http.ContentType;
+import java.util.List;
+import javax.persistence.EntityManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class MealResourceTest extends SetupRestTests {
+
+    private static Meal m1, m2, m3;
+
+    @BeforeAll
+    static void setUpClass() {
+        setupServer();
+    }
+
+    @AfterAll
+    static void closeTestServer() {
+        shutdownServer();
+    }
+
+    @BeforeEach
+    void setUp() {
+        m1 = new Meal("Chicken", 40, Category.STICK, "this is an awesome chicken");
+        m2 = new Meal("Some Maki", 60, Category.MAKI, "Get your own Maki!");
+        m3 = new Meal("Sashimi sushi", 60, Category.SASHIMI, "Some Sashimi has never hurt anyone");
+
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(m1);
+            em.persist(m2);
+            em.persist(m3);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.createNamedQuery("Meal.deleteAllRows").executeUpdate();
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    @Nested
+    @DisplayName("get all")
+    class GetAll {
+
+        @Test
+        @DisplayName("should get all of the meals that we have")
+        void shouldGetAllOfTheMealsThatWeHave() {
+            given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/meals")
+                .then()
+                .statusCode(200);
+        }
+
+        @Test
+        @DisplayName("should have a restaurant name on in")
+        void shouldHaveARestaurantNameOnIn() {
+            given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/meals")
+                .then()
+                .body("name", equalTo("Sushi Lovers"));
+        }
+
+        @Test
+        @DisplayName("should have a list of meals on it")
+        void shouldHaveAListOfMealsOnIt() {
+            List<MealDTO> meals = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/meals")
+                .then()
+                .extract().body().jsonPath().getList("meals", MealDTO.class);
+
+            assertFalse(meals.isEmpty());
+        }
+    }
+
+    @Nested
+    @DisplayName("generate all meals")
+    class GenerateAllMeals {
+
+        @Test
+        @DisplayName("should generate meals")
+        void shouldGenerateMeals() {
+            given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/meals/generate")
+                .then()
+                .statusCode(200);
+        }
+    }
+
+}

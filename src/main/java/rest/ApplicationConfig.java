a40c2cbc4e614e64133cf74952c55ed2ff6f24f1
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2021-05-15 13:13:54 +0200

    fix: added order-resource into application-config

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -28,6 +28,7 @@ public class ApplicationConfig extends Application {
         resources.add(rest.DemoResource.class);
         resources.add(rest.RenameMeResource.class);
         resources.add(rest.MealResource.class);
+        resources.add(rest.OrderResource.class);
         resources.add(security.JWTAuthenticationFilter.class);
         resources.add(security.LoginEndpoint.class);
         resources.add(security.RolesAllowedFilter.class);
